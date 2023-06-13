3d0266ffd5b39e8a4d3e6388aa1fcb70b70b5f6c
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-06-13 21:01:59 +0330

    Update project name

--- MessangerServer/asgi.py
+++ MessangerServer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DNS_Server project.
+ASGI config for MessangerServer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DNS_Server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MessangerServer.settings')
 
 application = get_asgi_application()
--- MessangerServer/urls.py
+++ MessangerServer/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DNS_Server project.
+URL configuration for MessangerServer project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
