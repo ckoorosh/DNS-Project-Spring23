02ad4d0f1817aef51effe622da165275d6942a8b
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-06-30 20:22:32 +0330

    Fix websocket timeout issue

--- MessangerServer/consumers.py
+++ MessangerServer/consumers.py
@@ -1,9 +1,10 @@
+import asyncio
 import json
 import os
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-from MessangerServer.utlis import JwtUtil, Singleton
+from MessangerServer.utlis import JwtUtil
 from MessangerServer.websocket_manager import WebsocketManager
 
 cookie_name = os.getenv('WS_AUTH_COOKIE_NAME')
@@ -12,26 +13,45 @@ cookie_name = os.getenv('WS_AUTH_COOKIE_NAME')
 class WSConsumer(AsyncWebsocketConsumer):
     user_id: str
     token: str
+    ping_interval: int = 10
+    connected: bool
 
     async def connect(self):
         token = self.scope['cookies'][cookie_name]
-        user_id = self.scope['url_route']['kwargs']['user_id']
+        user_id = self.scope['url_route']['kwargs']['username']
         decoded_token = JwtUtil().jwt_decode(token)
-        if decoded_token['user_id'] != user_id:
+        if decoded_token['username'] != user_id:
             raise Exception('Unauthorized user')
         self.user_id = user_id
         self.token = token
         await self.channel_layer.group_add(self.user_id, self.channel_name)
         await self.accept()
         WebsocketManager().add_group(user_id)
+        self.connected = True
+        asyncio.create_task(self.schedule_ping())
 
     async def disconnect(self, close_code):
+        self.connected = False
         await self.channel_layer.group_discard(self.user_id, self.channel_name)
         WebsocketManager().remove_group(self.user_id)
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
-        await self.disconnect(close_code=111)
         pass
 
     async def send_message(self, message):
-        await self.send(text_data=json.dumps({'message': message['message']}))
+        to_send = {
+            'type': 'message',
+            'message': message
+        }
+        await self.send(text_data=json.dumps(to_send))
+
+    async def send_bytes(self, bytes_data):
+        await self.send(bytes_data=bytes_data)
+
+    async def schedule_ping(self):
+        while True:
+            await asyncio.sleep(self.ping_interval)
+            if self.connected:
+                await self.send(text_data=json.dumps({'type': 'ping'}))
+            else:
+                break
--- MessangerServer/websocket_manager.py
+++ MessangerServer/websocket_manager.py
@@ -17,20 +17,34 @@ class WebsocketManager(metaclass=Singleton):
     def get_connected_user_ids(self):
         return list(self.groups)
     
-    def is_user_online(self, user_id):
-        return str(user_id) in self.groups
+    def is_user_online(self, username):
+        return str(username) in self.groups
 
-    async def send_message_to_user(self, user_id, message):
-        if str(user_id) not in self.groups:
+    async def send_message_to_user(self, username, message):
+        if not self.is_user_online(username):
             return False
 
         channel_layer = get_channel_layer()
 
         await channel_layer.group_send(
-            str(user_id),
+            str(username),
             {
                 'type': 'send_message',
                 'message': message,
+            }
+        )
+        return True
+
+    async def send_bytes_to_user(self, username, bytes_data):
+        if not self.is_user_online(username):
+            return False
+        channel_layer = get_channel_layer()
+
+        await channel_layer.group_send(
+            str(username),
+            {
+                'type': 'send_bytes',
+                'bytes_data': bytes_data,
             }
         )
         return True
